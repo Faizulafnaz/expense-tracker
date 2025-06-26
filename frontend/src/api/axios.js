b790894824c96dadd44a3dc88bbb91ecfaa7e700
Faizulafnaz <faizulafnaz22@gmail.com>
2025-06-26 13:05:29 +0530

    ADDED .env in frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const apiUrl = import.meta.env.VITE_API_BASE_URL;
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: apiUrl,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
